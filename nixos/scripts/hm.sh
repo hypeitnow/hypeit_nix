abf0496f18ca1d48ff8aa0f3147ed197b96b0a9e
hypeit <47086634+hypeitnow@users.noreply.github.com>
2023-07-02 17:49:25 +0000

    added cachix install

--- nixos/scripts/hm.sh
+++ nixos/scripts/hm.sh
@@ -1,5 +1,6 @@
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz 
 sudo nix-channel --update
 
+sudo nix-env -iA cachix -f https://cachix.org/api/v1/install
 sudo cachix authtoken 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlZGU3OGYwZC0yOGExLTQ4YmMtOTVhZS1mMDk5OTA5ZjFkYzIiLCJzY29wZXMiOiJjYWNoZSJ9.J3De57Lrec7TU2MiQk24-Sg9mRELEI4J3d0IWPQm-tI'
 sudo cachix watch-store hypeitnow
