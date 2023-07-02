c49c0a3500c83025f4ec9e3aba48c9617ddc042a
Hype It <matkoz111@gmail.com>
2023-07-02 20:56:36 +0200

    version bump

--- nixos/modules/rc505.nix
+++ nixos/modules/rc505.nix
@@ -4,14 +4,14 @@
   # enable the Roland RC-505 as an ALSA device
 
   # pin the kernel so we don't need to keep building it
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_5_15.override {
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_1.override {
     argsOverride = rec {
       src = pkgs.fetchurl {
-        url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-        sha256 = "sha256-2pJw2+ZN3x2xPHBHCVf/Z5brmW2Ge7Su19FKcOHGWnI=";
+        url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+        sha256 = "sha256-6GkXu6GZDpZ5Q2RUhBgqZLoyX5ixFKGQbMHVCZLgc8E=";
       };
-      version = "5.15.94";
-      modDirVersion = "5.15.94";
+      version = "6.1.31";
+      modDirVersion = "6.1.31";
     };
   });
 
