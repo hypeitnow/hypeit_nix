7a7eaca7b8b8eacc1345ad322e3fc9db3d85154d
hypeit <47086634+hypeitnow@users.noreply.github.com>
2023-07-02 09:36:28 +0200

    Update hm.sh
    add sudo

--- nixos/scripts/hm.sh
+++ nixos/scripts/hm.sh
@@ -1,5 +1,5 @@
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz 
 sudo nix-channel --update
 
-cachix authtoken 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlZGU3OGYwZC0yOGExLTQ4YmMtOTVhZS1mMDk5OTA5ZjFkYzIiLCJzY29wZXMiOiJjYWNoZSJ9.J3De57Lrec7TU2MiQk24-Sg9mRELEI4J3d0IWPQm-tI'
-cachix watch-store hypeitnow
+sudo cachix authtoken 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlZGU3OGYwZC0yOGExLTQ4YmMtOTVhZS1mMDk5OTA5ZjFkYzIiLCJzY29wZXMiOiJjYWNoZSJ9.J3De57Lrec7TU2MiQk24-Sg9mRELEI4J3d0IWPQm-tI'
+sudo cachix watch-store hypeitnow
