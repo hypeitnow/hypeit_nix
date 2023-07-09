44e6002b775bd1da5603a6bd9a1f85e3996bcb81
Hype It <matkoz111@gmail.com>
2023-07-09 23:23:17 +0200

    do not use cache

--- nixos/scripts/hm.sh
+++ nixos/scripts/hm.sh
@@ -3,5 +3,5 @@ sudo nix-channel --update
 
 sudo nix-env -iA cachix -f https://cachix.org/api/v1/install
 sudo cachix authtoken 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlZGU3OGYwZC0yOGExLTQ4YmMtOTVhZS1mMDk5OTA5ZjFkYzIiLCJzY29wZXMiOiJjYWNoZSJ9.J3De57Lrec7TU2MiQk24-Sg9mRELEI4J3d0IWPQm-tI'
-sudo cachix use hypeitnow
+#sudo cachix use hypeitnow
 sudo cachix watch-store hypeitnow
